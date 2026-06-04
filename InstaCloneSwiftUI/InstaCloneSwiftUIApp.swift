e352c99003c35b46fbf833acdee397dbe6ec8afb
Ömer Apaydın <omerapaydin90@gmail.com>
2026-06-04 20:25:47 +0300

    Initial Commit

---
+++ InstaCloneSwiftUI/InstaCloneSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstaCloneSwiftUIApp.swift
+//  InstaCloneSwiftUI
+//
+//  Created by Ömer Apaydın on 4.06.2026.
+//
+
+import SwiftUI
+
+@main
+struct InstaCloneSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
