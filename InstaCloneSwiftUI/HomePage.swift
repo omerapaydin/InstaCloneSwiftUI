5c53cf1a1eee9f0ff7c98e9e76b5463249f775a4
Ömer Apaydın <omerapaydin90@gmail.com>
2026-06-05 02:06:29 +0300

    signIn ayarlandı

---
+++ InstaCloneSwiftUI/HomePage.swift
@@ -0,0 +1,18 @@
+//
+//  HomePage.swift
+//  InstaCloneSwiftUI
+//
+//  Created by Ömer Apaydın on 5.06.2026.
+//
+
+import SwiftUI
+
+struct HomePage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomePage()
+}
