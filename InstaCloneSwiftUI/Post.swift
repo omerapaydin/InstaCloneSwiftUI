03dfb62945bb2424cc39b9a0c89eeac9fc8b0f39
Ömer Apaydın <omerapaydin90@gmail.com>
2026-06-05 21:19:36 +0300

    Firebase post getirme

---
+++ InstaCloneSwiftUI/Post.swift
@@ -0,0 +1,8 @@
+//
+//  Post.swift
+//  InstaCloneSwiftUI
+//
+//  Created by Ömer Apaydın on 5.06.2026.
+//
+
+import Foundation
---
+++ InstaCloneSwiftUI/PostViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  PostViewModel.swift
+//  InstaCloneSwiftUI
+//
+//  Created by Ömer Apaydın on 5.06.2026.
+//
+
+import Foundation
